80246dedefbe6036096da0d89a9365504318b3cd
Karan Batra <mrbatrakaran@gmail.com>
2022-10-20 23:55:36 -0400

    added top charts and artists pages

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -14,6 +14,9 @@ export const shazamCoreApi = createApi({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
     getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
     getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+    useGetTopChartsQuery: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
   }),
 });
 
@@ -21,4 +24,6 @@ export const {
   useGetTopChartsQuery,
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
 } = shazamCoreApi;
