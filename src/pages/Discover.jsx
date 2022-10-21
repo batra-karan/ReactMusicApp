06606ebf29f4b95608ea82d7754f92cd4929ed4c
Karan Batra <mrbatrakaran@gmail.com>
2022-10-21 00:41:19 -0400

    quick fixes

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -5,19 +5,16 @@ import { genres } from '../assets/constants';
 import { useGetSongsByGenreQuery } from '../redux/services/shazamCore';
 import { selectGenreListId } from '../redux/features/playerSlice';
 
-
-
 const Discover = () => {
   const dispatch = useDispatch();
-  const { activeSong, isPlaying, genreListId } = useSelector((state) =>  state.player);
-
+  const { activeSong, isPlaying, genreListId } = useSelector((state) => state.player);
 
   const { data, isFetching, error } = useGetSongsByGenreQuery(genreListId || 'POP');
-  
+
   if (isFetching) return <Loader title="Loading Song..." />;
-  
+
   if (error) return <Error />;
-  const genresTitle = genres.find(({value}) => value === genreListId)?.title;
+  const genresTitle = genres.find(({ value }) => value === genreListId)?.title;
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -36,8 +33,8 @@ const Discover = () => {
           <SongCard
             key={song.key}
             song={song}
-            isPlaying = {isPlaying}
-            activeSong = {activeSong}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
             data={data}
             i={i}
           />
